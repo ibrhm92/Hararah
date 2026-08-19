59f83abe68ef10c9e9afc3d37dcb188007050abd
Ibrhm92 <ibrhmhq92@gmail.com>
2026-08-19 17:40:14 +0300

    إصلاح ملفات Gradle Wrapper وتحديث إعدادات البناء في GitHub Actions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,13 +20,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug") // Change for production release signing
+            signingConfig = signingConfigs.getByName("debug")
         }
         debug {
             isMinifyEnabled = false
@@ -35,12 +34,12 @@ android {
     }
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
